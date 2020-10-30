c0c74dea14be293120d32ab11c83a0890526cf65
unknown <alexomosa001@gmail.com>
2020-10-30 18:58:19 +0300

    Message

---
+++ src/components/map/MapCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MapCard() {
+  return <div>MAP</div>;
+}
+
+export default MapCard;
--- src/components/map/SearchMap.js
+++ src/components/map/SearchMap.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import MapCard from "./../../img/home.svg";
 import Spinner from "./Spinner";
 import MapStyles from "./MapStyles";
 const libraries = ["places"];
@@ -15,6 +16,12 @@ const options = {
   styles: MapStyles,
   disableDefaultUI: true,
 };
+const markers = [
+  {
+    lat: -1.292066,
+    lng: 36.821945,
+  },
+];
 function SearchMap() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -29,7 +36,20 @@ function SearchMap() {
         zoom={14}
         center={center}
         options={options}
-      ></GoogleMap>
+      >
+        {markers.map((marker, i) => (
+          <Marker
+            key={i}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: MapCard,
+              scaledSize: new window.google.maps.Size(40, 40),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(20, 20),
+            }}
+          />
+        ))}
+      </GoogleMap>
     </div>
   );
 }
